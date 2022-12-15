a4d84c2619848294adffec580b7cf979e93ab3c0
Harrison-Dev <hao0628@gmail.com>
2022-12-16 00:48:18 +0800

    add build sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ls -al;
+echo "start build";
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
+bash Miniconda3-latest-Linux-x86_64.sh;
+conda env create -f environment.yml; 
+conda activate chat; 
+playwright install; 
+playwright install-deps; 
+python server.py
