a7219c5675d8b376098fbe9784ad831883c022b1
Harrison-Dev <hao0628@gmail.com>
2022-12-16 01:18:27 +0800

    give up use conda

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 ls -al;
 echo "start build";
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
-yes | bash Miniconda3-latest-Linux-x86_64.sh;
-conda env create -f environment.yml; 
-conda activate chat; 
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
+# bash Miniconda3-latest-Linux-x86_64.sh;
+# conda env create -f environment.yml; 
+# conda activate chat; 
 playwright install; 
 playwright install-deps; 
-python server.py
+python3 server.py
