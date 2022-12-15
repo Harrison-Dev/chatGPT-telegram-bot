92fb646b7e3e23263d6b140004e2e2911d671a68
Harrison-Dev <hao0628@gmail.com>
2022-12-16 00:57:18 +0800

    auto answer yes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ls -al;
 echo "start build";
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
-bash Miniconda3-latest-Linux-x86_64.sh;
+yes | bash Miniconda3-latest-Linux-x86_64.sh;
 conda env create -f environment.yml; 
 conda activate chat; 
 playwright install; 
